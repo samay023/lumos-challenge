c88c490c800eddd878557eedff4efabf4069c887
Pavan Raju <pavan@spotlumos.com>
2025-03-06 15:03:49 +1100

    HR-000: add JS/TS exercise

---
+++ tests/bank.test.ts
@@ -0,0 +1,15 @@
+// tests/calculator.test.ts
+import { Bank } from '../src/bank';
+
+describe('Bank', () => {
+    let bank: Bank;
+
+    beforeEach(() => {
+        // create a bank
+    });
+
+    test('true to be true', () => {
+        expect(true).toBe(true);
+    });
+
+});
---
+++ tests/library.test.ts
@@ -0,0 +1,15 @@
+
+import { Library } from '../src/library';
+
+describe('Library', () => {
+    let bank: Library;
+
+    beforeEach(() => {
+        // create a library
+    });
+
+    test('true to be true', () => {
+        expect(true).toBe(true);
+    });
+
+});
